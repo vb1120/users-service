7e3490cb68e800c2a6c55a23ca8ad60b6791dec3
vb1120 <bekmuradovimprove@gmail.com>
2022-07-05 17:51:19 +0500

    Singleton for Express router implement

---
+++ src/utils/AppRouter.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+// This is Singleton class for express router
+export class AppRouter {
+    private static instance: express.Router
+
+    static getInstance(): express.Router {
+        if (!AppRouter.instance) AppRouter.instance = express.Router()
+        return AppRouter.instance
+    }
+}
