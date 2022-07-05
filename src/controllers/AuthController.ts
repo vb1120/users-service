8683f444ad9c52d0c2a3cf35388a4cfcd3d96292
vb1120 <bekmuradovimprove@gmail.com>
2022-07-05 18:14:51 +0500

    AuthController class init

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+
+class AuthController {
+    async signup(req: Request, res: Response) {}
+    async login(req: Request, res: Response) {}
+    async token(req: Request, res: Response) {}
+    async logout(req: Request, res: Response) {}
+}
