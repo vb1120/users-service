99ca628db1fc5f4a057410bc5b383fd3986f4cac
vb1120 <bekmuradovimprove@gmail.com>
2022-07-05 18:46:55 +0500

    Signup logic implement

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from 'express'
+import { User } from '../models'
+import { post } from './decorators'
+import { controller } from './decorators/controller'
 
+@controller('/')
 class AuthController {
-    async signup(req: Request, res: Response) {}
+    @post('signup')
+    async signup(req: Request, res: Response) {
+        const { email, password } = req.body
+
+        const user = await User.findOne({ where: { email } })
+        if (user)
+            return res
+                .status(400)
+                .send({ msg: `User with email '${email}' already exists` })
+
+        const newUser = await User.create(req.body)
+
+        const { accessToken, refreshToken } =
+            newUser.assignTokensToUserAndReturnThem()
+
+        await newUser.createRefreshToken({ token: refreshToken })
+
+        return res.status(201).send({ accessToken, refreshToken })
+    }
     async login(req: Request, res: Response) {}
     async token(req: Request, res: Response) {}
     async logout(req: Request, res: Response) {}
