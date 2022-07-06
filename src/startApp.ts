ad4d1252e4dbab434d41ac9eb6e870b4a9670d32
vb1120 <bekmuradovimprove@gmail.com>
2022-07-07 01:36:10 +0500

    Swagger doc complete

--- src/startApp.ts
+++ src/startApp.ts
@@ -4,6 +4,8 @@ import express from 'express'
 import morgan from 'morgan'
 import { errorHandler } from './middlewares'
 import { AppRouter, envs } from './utils'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDoc from './swagger.json'
 
 const { port } = envs
 
@@ -11,6 +13,7 @@ export const startApp = () => {
     const app = express()
 
     app.use(cors())
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
     app.use(bodyParser.json())
     app.use(morgan('combined'))
 
