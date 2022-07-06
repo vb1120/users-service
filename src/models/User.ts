78c5cb21036c5679c09cee8495c781e344e6c85d
vb1120 <bekmuradovimprove@gmail.com>
2022-07-06 17:53:44 +0500

    Send user info to message queue in after create hook implement

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,10 @@ import {
     HasOneGetAssociationMixin,
     HasOneCreateAssociationMixin,
     HasOneSetAssociationMixin,
-    DataTypes,
-    Association
+    DataTypes
 } from 'sequelize'
 import { sequelize } from '../db'
+import { MessageBroker } from '../utils'
 import { IJwtPayload, JwtUtils } from '../utils/JwtUtils'
 import { hashPassword } from '../utils/passwordUtils'
 import { RefreshToken } from './RefreshToken'
@@ -78,10 +78,30 @@ User.init(
     }
 )
 
-// Hooks
+// Hash passsword before create
 User.beforeCreate(async (user, options) => {
     user.password = await hashPassword(user.password)
 })
+
+// Hash passsword before update if password changed
 User.beforeUpdate(async (user, options) => {
     user.password = await hashPassword(user.password)
+})
+
+/*  
+    When user after create hook is triggered it sends 
+    the user email to mailing service through message broker
+ */
+User.afterCreate(async (user, options) => {
+    const messageBroker = await MessageBroker.getInstance()
+
+    const jsonToSend: { [key: string]: string } = {
+        email: user.email
+    }
+
+    if (user.name) jsonToSend['name'] = user.name
+
+    await messageBroker.send('mailing', Buffer.from(JSON.stringify(jsonToSend)))
+
+    await messageBroker.close()
 })
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import { initConnection, sequelize } from './db'
 import { startApp } from './startApp'
 import './controllers'
+import { MessageBroker } from './utils'
 
 initConnection(sequelize)
     .then(() => {
         startApp()
     })
     .catch((err: Error) => console.log(err.message))
+
+process.on('beforeExit', async () => {
+    const amqp = await MessageBroker.getInstance()
+    console.log('closing')
+    await amqp.close()
+})
